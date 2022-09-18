dd0e293920dcf1e2d901e33ae812170b64ef284d
RehamHesham224 <rehamhesham680@gmail.com>
2022-09-18 11:42:17 +0200

    make a design pattern test code for bridge

--- Tests/BridgeTest.php
+++ Tests/BridgeTest.php
@@ -1,6 +1,22 @@
 <?php
+namespace Tests;
 
-class BridgeTEst
+use \PHPUnit\Framework\TestCase;
+use Strctural\Bridge\BENZCar;
+use Strctural\Bridge\BlueCar;
+use Strctural\Bridge\BMWCar;
+use Strctural\Bridge\RedCar;
+
+class BridgeTest extends TestCase
 {
-
+    public function testCanCreateBlueBMWCar(){
+        $carColor=new BlueCar();
+        $BMWCar=new BMWCar($carColor);
+        $this->assertEquals("the car type is BMW and the car color is blue",$BMWCar->getProduct());
+    }
+    public function testCanCreateRedBENZCar(){
+        $carColor=new RedCar();
+        $BENZCar=new BENZCar($carColor);
+        $this->assertEquals("the car type is BENZ and the car color is red",$BENZCar->getProduct());
+    }
 }
