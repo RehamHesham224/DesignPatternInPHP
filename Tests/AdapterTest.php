2c3625ec45c941ea362995bbc08f4d6b8171c02a
RehamHesham224 <rehamhesham680@gmail.com>
2022-09-18 10:47:04 +0200

    make a design pattern test code for adapter

--- Tests/AdapterTest.php
+++ Tests/AdapterTest.php
@@ -1,6 +1,22 @@
 <?php
+namespace Tests;
+use \PHPUnit\Framework\TestCase;
+use Strctural\Adapter\Car;
+use Strctural\Adapter\EngineAdapter;
+use Strctural\Adapter\NormalEngine;
+use Strctural\Adapter\TurboEngine;
 
-class AdapterTest
+class AdapterTest extends TestCase
 {
-
+    public function testCanStaryNormalEngine(){
+        $engine=new NormalEngine();
+        $car=new Car($engine);
+        $this->assertEquals("normal Engine",$car->start());
+    }
+    public function testCanStaryTurboEngine(){
+        $engine=new TurboEngine();
+        $adapter=new EngineAdapter($engine);
+        $car=new Car($adapter);
+        $this->assertEquals("Turbo Engine",$car->start());
+    }
 }
