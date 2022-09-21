605015bf0fe6f0ba15b259fe31b9f56aef87099d
RehamHesham224 <rehamhesham680@gmail.com>
2022-09-21 06:19:43 +0200

    code example for dependancy injection design pattern

--- Structural/DependencyInjection/DatabaseConfig.php
+++ Structural/DependencyInjection/DatabaseConfig.php
@@ -4,5 +4,59 @@ namespace Strctural\DependencyInjection;
 
 class DatabaseConfig
 {
+    private string $host;
+    private string $username;
+    private string $password;
+    private string $port;
+    private string $databaseName;
 
+    public function __construct(string $host, string $username, string $password, string $port, string $databaseName)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->port = $port;
+        $this->databaseName = $databaseName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getDatabaseName(): string
+    {
+        return $this->databaseName;
+    }
 }
--- Structural/DependencyInjection/DatabaseConnection.php
+++ Structural/DependencyInjection/DatabaseConnection.php
@@ -2,7 +2,21 @@
 
 namespace Strctural\DependencyInjection;
 
-class DAtabaseConnection
+class DatabaseConnection
 {
+    private DatabaseConfig $config;
 
+    public function __construct(DatabaseConfig $config)
+    {
+        $this->config = $config;
+    }
+    public function getConnectionToString(){
+        return sprintf('mysql://%s:%s@%s:%s/%s',
+        $this->config->getUsername(),
+        $this->config->getPassword(),
+        $this->config->getHost(),
+        $this->config->getPort(),
+        $this->config->getDatabaseName()
+        );
+    }
 }
