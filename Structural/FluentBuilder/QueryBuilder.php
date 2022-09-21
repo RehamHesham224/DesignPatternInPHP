0dd1c870282eb4b37b6d79bbb6f56c57a1b92e56
RehamHesham224 <rehamhesham680@gmail.com>
2022-09-21 06:21:33 +0200

    code example for fluent builder design pattern

--- Structural/FluentBuilder/QueryBuilder.php
+++ Structural/FluentBuilder/QueryBuilder.php
@@ -2,7 +2,37 @@
 
 namespace Strctural\FluentBuilder;
 
-class QueryBuilder
+class QueryBuilder implements QueryBuilderInterface
 {
+    private array $columns;
+    private array $conditions;
+    private string $table;
+    private string $alias;
 
+    public function select(array $columns):self
+    {
+        $this->columns = $columns;
+        return $this;
+    }
+
+    public function from(string $table, string $alias):self
+    {
+        $this->table = $table;
+        $this->alias = $alias;
+        return $this;
+    }
+
+    public function where(array $conditions):self
+    {
+        $this->conditions = $conditions;
+        return $this;
+    }
+    public function getSQL(){
+        return sprintf('SELECT %s FROM %s As %s WHERE %s ',
+            implode(', ', $this->columns),
+            $this->table,
+            $this->alias,
+            implode(' AND ', $this->conditions)
+        );
+    }
 }
--- Structural/FluentBuilder/QueryBuilderInterface.php
+++ Structural/FluentBuilder/QueryBuilderInterface.php
@@ -4,5 +4,7 @@ namespace Strctural\FluentBuilder;
 
 interface QueryBuilderInterface
 {
-
+    public function select(array $columns);
+    public function from(string $table ,string $alias);
+    public function where(array $conditions);
 }
