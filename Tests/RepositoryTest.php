48e26b0b74945d991163fe6c3caf9ba1e46c1f1a
RehamHesham224 <rehamhesham680@gmail.com>
2022-09-25 11:10:47 +0200

    add a code test example of repository design pattern

--- Tests/RepositoryTest.php
+++ Tests/RepositoryTest.php
@@ -2,7 +2,41 @@
 
 namespace Tests;
 
-class RepositoryTest
-{
+use Others\Repository\ArrayEntityManager;
+use Others\Repository\PersistenceInterface;
+use Others\Repository\User;
+use Others\Repository\UserDataMapper;
+use Others\Repository\UserRepository;
+use Others\Repository\UserRepositoryInterface;
+use PHPUnit\Framework\TestCase;
 
+class RepositoryTest extends TestCase
+{
+    private PersistenceInterface $persistence;
+    private UserRepositoryInterface $userRepository;
+    protected function setUp(): void
+    {
+        $dataSource=[
+            1=>['name'=>'foo', 'email'=>'foo@bar.com','password'=>'foo@bar.com']
+        ];
+        $this->persistence=new ArrayEntityManager($dataSource);
+        $this->userRepository=new UserRepository($this->persistence,new UserDataMapper());
+    }
+    public function testCanFindUserById()
+    {
+        $user=$this->userRepository->find(1);
+        self::assertInstanceOf(User::class,$user);
+        self::assertEquals('foo',$user->getName());
+    }
+    public function testCanSaveUserById()
+    {
+      $user=new User();
+      $user->setEmail('rehamhesham680@gmail.com')
+      ->setPassword('###')
+      ->setName('Reham');
+      self::assertTrue($this->userRepository->save($user));
+      $newUser=$this->userRepository->find(2);
+      self::assertInstanceOf(User::class, $newUser);
+      self::assertEquals('Reham',$newUser->getName());
+    }
 }
